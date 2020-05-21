900dc45bce89f8edd20d1fd0a91f39f939fdbb09
antoniocassano84 <antonio.cassano984@gmail.com>
2020-05-21 06:19:06 +0100

    ItemDaoFileTest added

---
+++ src/test/java/cdlKata/datasource/ItemDaoFileTest.java
@@ -0,0 +1,33 @@
+package cdlKata.datasource;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemDaoFileTest {
+
+  @Test
+  void get() {
+    fail("TODO");
+  }
+
+  @Test
+  void getAll() {
+    fail("TODO");
+  }
+
+  @Test
+  void save() {
+    fail("TODO");
+  }
+
+  @Test
+  void update() {
+    fail("TODO");
+  }
+
+  @Test
+  void delete() {
+    fail("TODO");
+  }
+}
